74a75d215c9f1d5f1fda2a954495c04bb1226e0e
jdingus93 <jdingus93@yahoo.com>
2025-05-12 11:38:20 -0500

    Updated handler_agg.go to pull from multiple urls instead of a single to create a feed

--- handler_agg.go
+++ handler_agg.go
@@ -3,13 +3,50 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return fmt.Errorf("couldn't fetch feed: %w", err)
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("expected 1 agrument: time_between_reqs")
 	}
-	fmt.Printf("Feed: %+v\n", feed)
+
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("invalid duration format: %w", err)
+	}
+
+	fmt.Printf("Collecting feeds every %s\n", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		if err := scrapeFeeds(s); err != nil {
+			fmt.Printf("Error scraping feeds: %v\n", err)
+		}
+	}
+
 	return nil
+}
+
+func scrapeFeeds(s *state) error {
+    feed, err := s.db.GetNextFeedToFetch(context.Background())
+    if err != nil {
+        return err
+    }
+    
+    feedContent, err := fetchFeed(context.Background(), feed.Url)
+    if err != nil {
+        return err
+    }
+
+    err = s.db.MarkFeedFetched(context.Background(), feed.ID)
+    if err != nil {
+        return err
+    }
+    
+    for _, item := range feedContent.Channel.Item {
+        fmt.Printf("- %s\n", item.Title)
+    }
+    
+    return nil
 }
